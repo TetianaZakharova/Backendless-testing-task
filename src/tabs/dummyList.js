209b4af52bbe0978ae57fa3e4621be13937c2e9d
TetianaZakharova <kazakova.tanya@gmail.com>
2023-10-27 21:16:46 +0300

    create repo

---
+++ src/tabs/dummyList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DummyList = () => {
+  return (
+    <div>
+      <h2>Dummy List</h2>
+    </div>
+  );
+};
+
+export default DummyList;
---
+++ src/tabs/dummyTable.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DummyTable = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td><h2>Dummy</h2></td>
+          <td><h2>Table</h2></td>
+        </tr>
+      </thead>
+    </table>
+  );
+};
+
+export default DummyTable;
